f92dc0ebd510c0f0f827d46b61187bed31cfb79a
javier-troconis-q2 <javier.troconis@q2ebanking.com>
2019-04-14 13:36:26 -0500

    init

---
+++ IdentityServer.AzureFunction/src/IdentityServer.AzureFunction/X.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using IdentityServer.AzureFunction;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: WebJobsStartup(typeof(X))]
+namespace IdentityServer.AzureFunction
+{
+	internal class X : IWebJobsStartup
+	{
+
+		public void Configure(IWebJobsBuilder builder)
+		{
+			var r = Directory.GetParent(typeof(X).Assembly.Location).FullName;
+			
+			
+		}
+
+		
+	}
+}
